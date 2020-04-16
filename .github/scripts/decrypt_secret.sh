c352d4caac75b18ef9e72a9852d6e3011173018c
Vitor Delfino <vitor.delfino952@gmail.com>
2020-04-16 11:49:32 -0300

    change environents

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -3,5 +3,5 @@
 # Decodifica o arquivo
 mkdir $HOME/secrets
 # --coloca em lote para impedir comando interativo --sim para afirmar "sim" para perguntas
-gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
+gpg --quiet --batch --yes --decrypt --passphrase="$DECRYPT_SECRET_PASSPHRASE" \
 --output $HOME/secrets/secret_key secret_key.gpg
